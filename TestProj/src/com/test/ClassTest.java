8fc00f58d5cda91517793e6656a41b9f07ce5a58
JackiewangGit <18720978427@163.com>
2018-03-16 10:26:52 +0800

    first commit

---
+++ TestProj/src/com/test/ClassTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class ClassTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sql = "select * from table_test where 1=1";
+		sql = sql.toUpperCase();
+		System.out.println("select nvl(count(1),0) "+sql.substring(sql.indexOf("FROM"), sql.length()));
+	}
+
+}
